c62c6bb68129ffac6c2e56abf3e91dee25525e4a
bradbau <775235261@qq.com>
2018-10-05 12:04:05 +0800

    20181005\不含链表类型的代码

--- source/linearList-2.c
+++ source/linearList-2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<malloc.h>
 //
 #define TRUE 1
 #define FALSE 0
@@ -15,3 +16,25 @@ typedef int ElemType;
 
 #define LIST_INIT_SIZE 100
 #define LISTINCREMENT 10
+
+typedef struct  ListNode{
+    ElemType data;
+    struct ListNode *next;
+}ListNode, *pListNode;
+
+int e;
+ListNode L;
+pListNode pL= &L;
+
+Status InitList(pListNode **pL){
+    *pL = (ListNode *)malloc(sizeof(ListNode)*LIST_INIT_SIZE);
+    (*pL)->data = 0;
+    (*pL)->next = NULL;
+    return OK;
+}
+
+Status DestroyList(pListNode **pL){
+    if(!pL) return ERROR;
+    (*pL)->data= 0;
+    
+}
